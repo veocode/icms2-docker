2194dceb8116471dc48e5753d62297cf6c29cda1
veocode <dev@inlinecms.com>
2020-11-12 11:54:03 +0500

    refactored init.sh

--- init.sh
+++ init.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 ICMS_REPO="https://github.com/instantsoft/icms2.git"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-if [[ $1 == "clear" ]]; then
-    echo "Cleaning installation..."
-    rm -rf $DIR/icms2
-    mkdir $DIR/icms2
-    echo "" > $DIR/icms2/.gitkeep
-    rm -rf $DIR/mysql/db/*
-    echo "" > $DIR/mysql/db/.gitkeep    
-    cp $DIR/vendor/compose.yml $DIR/docker-compose.yml
-    exit 0    
-fi
+MODE=${1:-"install"}
 
 declare -A envs 
 envs[VERSION]=2.13.1
@@ -34,55 +24,63 @@ prompts[PHPMYADMIN_INSTALL]="Install phpMyAdmin? (y/n)"
 prompts[PHPMYADMIN_PORT]="phpMyAdmin Port"
 
 order=(VERSION HTTP_PORT MYSQL_DATABASE MYSQL_USER MYSQL_PASSWORD MYSQL_ROOT_PASSWORD PHPMYADMIN_INSTALL PHPMYADMIN_PORT)
-if [[ $1 == "deploy" ]]; then
-    order=(HTTP_PORT MYSQL_DATABASE MYSQL_USER MYSQL_PASSWORD MYSQL_ROOT_PASSWORD PHPMYADMIN_INSTALL PHPMYADMIN_PORT)
-fi
 
 PHPMYADMIN_INSTALL=y
 
-echo ""
-echo -e "\e[96mWelcome to icms2-docker Installation Wizard\e[39m"
-echo "Please answer the questions to initialise your installation"
-echo ""
-
-for key in "${order[@]}"; do 
-    if [[ $key == "PHPMYADMIN_PORT" && $PHPMYADMIN_INSTALL != "y" ]]; then
-        continue
-    fi
-    default=${envs[$key]}
-    prompt=${prompts[$key]}
-    read -p "    $prompt "$'\e[2m['"$default"$']\e[22m: ' answer
-    answer=${answer:-$default}
-    envs[$key]=$answer
-    if [ $key == "PHPMYADMIN_INSTALL" ]; then
-        PHPMYADMIN_INSTALL=$answer
-    fi
-done
-
-echo ""
-
-if [[ $1 != "deploy" ]]; then
+clear_installation() {
     echo "Cleaning installation..."
     rm -rf $DIR/icms2
-    rm -rf $DIR/mysql/db/*
-    echo '' > $DIR/mysql/db/.gitkeep    
-fi
+    rm -rf $DIR/mysql/db
+    mkdir $DIR/icms2
+    mkdir $DIR/mysql/db
+    echo "" > $DIR/icms2/.gitkeep    
+    echo "" > $DIR/mysql/db/.gitkeep    
+    cp $DIR/vendor/compose.yml $DIR/docker-compose.yml   
+}
 
-echo "Saving configuration..."
-cp $DIR/vendor/compose.yml $DIR/docker-compose.yml
-rm -f $DIR/.env
-for key in "${order[@]}"; do 
-    if [[ $key == "PHPMYADMIN_PORT" && $PHPMYADMIN_INSTALL != "y" ]]; then
-        continue
+run_wizard() {
+    echo ""
+    echo -e "\e[96mWelcome to icms2-docker Installation Wizard\e[39m"
+    echo "Please answer the questions to initialise your installation"
+    echo ""
+
+    for key in "${order[@]}"; do 
+        if [[ $key == "VERSION" && $MODE != "install" ]]; then
+            continue
+        fi    
+        if [[ $key == "PHPMYADMIN_PORT" && $PHPMYADMIN_INSTALL != "y" ]]; then
+            continue
+        fi
+        default=${envs[$key]}
+        prompt=${prompts[$key]}
+        read -p "    $prompt "$'\e[2m['"$default"$']\e[22m: ' answer
+        answer=${answer:-$default}
+        envs[$key]=$answer
+        if [ $key == "PHPMYADMIN_INSTALL" ]; then
+            PHPMYADMIN_INSTALL=$answer
+        fi
+    done
+
+    echo ""
+}
+
+save_config() {
+    echo "Saving configuration..."
+    cp $DIR/vendor/compose.yml $DIR/docker-compose.yml
+    rm -f $DIR/.env
+    for key in "${order[@]}"; do 
+        if [[ $key == "PHPMYADMIN_PORT" && $PHPMYADMIN_INSTALL != "y" ]]; then
+            continue
+        fi
+        echo "$key=${envs[$key]}" >> $DIR/.env
+    done
+    if [[ $PHPMYADMIN_INSTALL == "y" ]]; then
+        cat $DIR/vendor/phpmyadmin.yml >> $DIR/docker-compose.yml
     fi
-    echo "$key=${envs[$key]}" >> $DIR/.env
-done
-if [[ $PHPMYADMIN_INSTALL == "y" ]]; then
-    cat $DIR/vendor/phpmyadmin.yml >> $DIR/docker-compose.yml
-fi
+}
 
-if [[ $1 != "deploy" ]]; then
-    VERSION="${envs[VERSION]}"
+download_icms() {
+    local VERSION="${envs[VERSION]}"
 
     echo "Downloading InstantCMS v$VERSION..."
     git clone -q --branch $VERSION $ICMS_REPO || { 
@@ -97,17 +95,52 @@ if [[ $1 != "deploy" ]]; then
     rm -f $DIR/icms2/README.md
     rm -f $DIR/icms2/README_RU.md
     rm -f $DIR/icms2/LICENSE
-fi
+}
 
-echo "Setting up permissions..."
-find $DIR/icms2/ -type f -exec chmod 644 {} \;
-find $DIR/icms2/ -type d -exec chmod 755 {} \;
-chmod -R 777 $DIR/icms2/upload
-chmod -R 777 $DIR/icms2/cache
-chmod 777 $DIR/icms2/system/config
+set_icms_permissions() {
+    echo "Setting up permissions..."
+    find $DIR/icms2/ -type f -exec chmod 644 {} \;
+    find $DIR/icms2/ -type d -exec chmod 755 {} \;
+    chmod -R 777 $DIR/icms2/upload
+    chmod -R 777 $DIR/icms2/cache
+    chmod 777 $DIR/icms2/system/config
+}
 
-echo "Starting Docker..."
-docker-compose up -d
+run_docker() {
+    echo "Starting Docker..."
+    docker-compose up -d
+}
 
-echo "Done!"
-echo " "
+completed() {
+    echo "Done!"
+    echo " "
+}
+
+main() {
+
+    if [[ $MODE == "install" ]]; then 
+        run_wizard
+        clear_installation
+        save_config
+        download_icms
+        set_icms_permissions
+        run_docker
+        completed
+    fi
+
+    if [[ $MODE == "deploy" ]]; then 
+        run_wizard
+        save_config
+        set_icms_permissions
+        run_docker
+        completed
+    fi
+
+    if [[ $MODE == "clear" ]]; then 
+        clear_installation
+        completed
+    fi
+
+}
+
+main $@
